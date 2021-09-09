97e0cc21056bb71ae7bede0222de3130fdeec925
Sergey <levashov04sergey@gmail.com>
2021-09-09 22:05:48 +0300

    play with theme for button,  create Navbar component

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import {Person} from "@material-ui/icons";
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+
+}));
+
+const App = () => {
+  const classes = useStyles();
+
+  return (
+      <div>
+
+      </div>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import {theme} from "./theme";
+import ThemeProvider from "@material-ui/styles/ThemeProvider";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <ThemeProvider theme={theme}>
+          <App />
+      </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
